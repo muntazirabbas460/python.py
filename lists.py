b33c4f81ac748dbcf8de8f10c6f68030390294ff
muntazirabbas460 <muntazirabbas0347@gmail.com>
2026-04-14 17:25:55 +0500

    Todays commit

---
+++ lists.py
@@ -0,0 +1,55 @@
+# list = [1, 2, 3,]
+# list.remove(1)
+# list.pop(1)
+# print(list)
+
+# get the names of three movies from user and print them in tha form of lists
+
+# movies=[]
+# movies1 = input("your favourite movie1 : ")
+# movies2 = input("your favourite movie2 : ")
+# movies3 = input("your favourite movie3 : ")
+
+# movies.append(movies1)
+# movies.append(movies2)
+# movies.append(movies3)
+# print(movies)
+
+# write a programme to count the grades
+# grades = ("A", "B", "C", "D", "A", "A", "A")
+# print(grades.count("B"))
+
+
+# store the above values in the list and store them form A to D
+
+# grades = ("A", "B", "C", "D", "A", "H")
+# grades.sort()
+# print(grades)
+
+# Dictionary in Python
+
+# info = {
+#               "key" : "value",
+#               "name" : "alam",
+#               "learning" : "codeing",
+#               "age" : 35
+
+# }
+
+# info["name"] = "akram"
+# null_dict = {}
+# null_dict["name"] = "sheen"
+# print(info)
+
+# Nested_dictionary
+
+student = {
+              "name" : "hussain",
+              "subjects" : {
+                            "physics" : 99,
+                            "App_dev" : 90,
+                            "Database" : 80,
+              }
+
+}
+print(student["subjects"] ["physics"])

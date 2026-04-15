5ad7ac490f2b97e69edd7df15eaefbe00aea77ec
muntazirabbas460 <muntazirabbas0347@gmail.com>
2026-04-15 21:07:17 +0500

    Aritmetic operators

---
+++ first.py
@@ -0,0 +1,18 @@
+# name = "muntazir"
+# age = 4496
+# price = 25
+
+# age2 = age
+
+# print ("my age is ", age2)
+
+# Arithmetic operators
+
+a = 5
+b = 2
+# sum = a + b
+print(a + b)
+print(a * b)
+print(a / b)
+print(a % b)
+

b81e9c417fdb575f1fc5ff65cc44fea327f68f41
muntazirabbas460 <muntazirabbas0347@gmail.com>
2026-04-22 11:18:58 +0500

    Assignment Operators

--- first.py
+++ first.py
@@ -8,11 +8,29 @@
 
 # Arithmetic operators
 
-a = 5
-b = 2
-# sum = a + b
-print(a + b)
-print(a * b)
-print(a / b)
-print(a % b)
+# a = 5
+# b = 2
+# # sum = a + b
+# print(a + b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
 
+
+# KEYWORDS
+
+# Relational operators
+
+a = 50
+b = 45
+
+print(a == b)
+print(a < b)
+print(a > b)
+print(a / b)
+
+# Assignment Operators
+
+num = 10
+num = num + 10
+print(num)

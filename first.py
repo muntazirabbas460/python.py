4606d7e754705d8330f65c82f37423700cf7e8a2
muntazirabbas460 <muntazirabbas0347@gmail.com>
2026-04-22 14:35:00 +0500

    prctice

--- first.py
+++ first.py
@@ -49,8 +49,42 @@
 # a = str(a)
 # print(type(a))
 
-a = 3.78
+# a = 3.78
 
-a = int(a)
+# a = int(a)
 
-print(type(a))
+# print(type(a))
+
+# name = input("enter your name: ")
+# print("welcome ", name)
+
+
+# write a programme in which you get the two numbers from as input from user and print its sum
+
+# num1 = int(input("num1 : "))
+# num2 = int(input("num2 : "))
+
+
+# print("sum = ", num1 + num2)
+
+# Write a programme to input side of  square & print its area
+
+# side_1 = int(input("val1 : "))
+
+
+# print("area of Square : ", side_1 * side_1)
+
+# WAP to to input 2 floting numbers & print their average
+
+# a = float(input("num1 : "))
+# b = float(input("num2 : "))
+
+# print("average : ", a + b / 2)
+
+# WAP to input two int numbers a and b. print True if a is greater 
+# than or equal to b. 
+
+a = int (input("num1 : "))
+b = int (input("num2 : "))
+
+print(a >= b)
